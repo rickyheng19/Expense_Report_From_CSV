142341111f0711ce6308e515da160ca2501c1825
rickyheng19 <rickyheng19@gmail.com>
2024-07-08 12:36:41 -0500

    Filtering CSV files and fixing the column names in the debit account works

--- ExpenseCombiner.py
+++ ExpenseCombiner.py
@@ -3,20 +3,27 @@ from pathlib import Path
 import pandas as pd
 import calendar
 import os
-import csv
 
 #Name file as last month
 today = datetime.now()
-lastMonth  = f"{calendar.month_name[today.month - 1]} {today.year} Expense Report"
+lastMonth = today.month - 1 if today.month > 1 else 12
+lastYear = today.year if today.month > 1 else today.year - 1
+lastMonthName  = f"{calendar.month_name[lastMonth]} {lastYear} Expense Report"
 
 #Make master expense sheet
-header = {"Transaction Date": [], "Description": [], "Category": [], "Amount": []}
-newFile = pd.DataFrame(header)
+header = ["Transaction Date", "Description", "Category", "Amount"]
+newFile = pd.DataFrame(columns=header)
 
 #go through each CVS file in folder
-for file in os.scandir():
+directory = Path('..') / 'CSV Bank Statements'
+for file in os.scandir(directory):
     if file.is_dir():
         continue
     CSVData = pd.read_csv(file)
-    print(CSVData.head())
+    if "Transaction Date" not in CSVData.columns or "Category" not in CSVData.columns:
+        if "Posting Date" in CSVData.columns or "Type" in CSVData.columns:
+            CSVData.rename(columns={"Posting Date": "Transaction Date"}, inplace=True)
+            CSVData.rename(columns={"Type": "Category"}, inplace=True)
+    filteredData = CSVData[header]
+    print(filteredData.head())
 
